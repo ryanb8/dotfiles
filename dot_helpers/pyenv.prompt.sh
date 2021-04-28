80911ec9b811d2b006db584a1ab5f51df65593d8
Ryan Boyer <ryanboyerpublic@gmail.com>
2021-04-28 12:59:22 -0500

    Use pipe not space for pyenv prompt

--- dot_helpers/pyenv.prompt.sh
+++ dot_helpers/pyenv.prompt.sh
@@ -3,7 +3,7 @@ function pyenv_prompt_update() {
     if command -v pyenv >/dev/null; then
         PYENV_VER=$(pyenv version-name)                                       # capture version name in variable
         if [[ "${PYENV_VER}" != "$(pyenv global | paste -sd ':' -)" ]]; then  # MODIFIED: "system" -> "$(pyenv global | paste -sd ':' -)"
-            export PYENV_PROMPT="(${PYENV_VER%%:*}) "                        # grab text prior to first ':' character
+            export PYENV_PROMPT="(${PYENV_VER%%:*})|"                        # grab text prior to first ':' character
         else
             export PYENV_PROMPT=""
         fi
--- dot_helpers/pyenv.prompt.zsh
+++ dot_helpers/pyenv.prompt.zsh
@@ -4,7 +4,7 @@ function pyenv_prompt_update() {
     if command -v pyenv >/dev/null; then
         PYENV_VER=$(pyenv version-name)                                       # capture version name in variable
         if [[ "${PYENV_VER}" != "$(pyenv global | paste -sd ':' -)" ]]; then  # MODIFIED: "system" -> "$(pyenv global | paste -sd ':' -)"
-            PYENV_PROMPT="(${PYENV_VER%%:*}) "                        # grab text prior to first ':' character
+            PYENV_PROMPT="(${PYENV_VER%%:*})|"                        # grab text prior to first ':' character
         fi
     fi
 }
