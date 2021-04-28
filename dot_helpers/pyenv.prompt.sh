fd034b27d78b0826c71c5346768496cc11cda3f2
Ryan Boyer <ryanboyerpublic@gmail.com>
2021-04-28 13:12:56 -0500

    Pyenv prompt for bash working

--- dot_helpers/pyenv.prompt.sh
+++ dot_helpers/pyenv.prompt.sh
@@ -3,11 +3,13 @@ function pyenv_prompt_update() {
     if command -v pyenv >/dev/null; then
         PYENV_VER=$(pyenv version-name)                                       # capture version name in variable
         if [[ "${PYENV_VER}" != "$(pyenv global | paste -sd ':' -)" ]]; then  # MODIFIED: "system" -> "$(pyenv global | paste -sd ':' -)"
-            export PYENV_PROMPT="(${PYENV_VER%%:*})|"                        # grab text prior to first ':' character
+            echo -n "(${PYENV_VER%%:*})|"                        # grab text prior to first ':' character
         else
-            export PYENV_PROMPT=""
+            echo -n ""
         fi
     else
-        export PYENV_PROMPT=""
-fi
+        echo -n ""
+    fi
 }
+
+PROMPT_COMMAND="pyenv_prompt_update;$PROMPT_COMMAND"
